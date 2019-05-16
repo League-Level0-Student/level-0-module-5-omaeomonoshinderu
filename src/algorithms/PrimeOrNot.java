bb6a81e1afcb43bee12ecf6864d50727278433ed
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-05-16 13:25:55 -0700

    Prime or not

--- src/algorithms/PrimeOrNot.java
+++ src/algorithms/PrimeOrNot.java
@@ -1,15 +1,23 @@
 package algorithms;
 
+import javax.swing.JOptionPane;
+
 public class PrimeOrNot {
+
 public static void main(String[] args) {
-	int number = 35;
-	int between = i;
+	String num = JOptionPane.showInputDialog("What number do you want to know is composite?");
+	int number = Integer.parseInt(num);
+	
+	
 	for (int i = 2; i < number; i++) {
-		boolean divisible = number/i=b;
-	if () {
 		
+	if (number%i==0) {
+		JOptionPane.showMessageDialog(null, "Your number is composite");
+	System.exit(0);
+	
 	}
-		
-	}		
+	
+	}	
+	JOptionPane.showMessageDialog(null, "Your number is composite");
 }
 }
